b848688971f974b7d91fab86ebc25750f6cb4b03
MM-Clark <142937692+MM-Clark@users.noreply.github.com>
2025-02-27 09:20:40 -0500

    SallySeashells mp3 added

--- STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/src/WinLoseScreen.java
+++ STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/src/WinLoseScreen.java
@@ -24,8 +24,11 @@ public class WinLoseScreen {
     }; //STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025\RedEyedFace.jpg
     private String winImagePath = "STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/testerImg.jpg"; 
 
-    // may add audio if have time here as path
-
+    // may add audio if have time here as path --> currently have two mp3 files 
+    // QuickFoxLazyDog is "The quick fox jumped over the lazy dog" distorted, reversed, equalized, slowed
+    // SallySeashells is "Sammy collects seashells by the seashores on Sundays" distorted, reversed, inverted, equalized, slowed
+    // -----------------> both files could use additional editing
+    // -----------------> JengaStackRec1_Reversed may be used again/edited diff. if lack time to make additional files
     public void showWinScreen(JFrame frame) throws IOException
     {
         // creating as BufferedImage to resize
