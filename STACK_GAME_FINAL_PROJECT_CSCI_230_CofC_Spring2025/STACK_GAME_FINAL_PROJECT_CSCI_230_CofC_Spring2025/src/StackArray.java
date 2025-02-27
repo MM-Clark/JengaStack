af878072dae19f229081c5bd571753e6bb76412d
Michelle Clark <142937692+MM-Clark@users.noreply.github.com>
2025-02-27 07:15:53 -0500

    Add files via upload
    Added img file for end screen.

---
+++ STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/src/StackArray.java
@@ -0,0 +1,57 @@
+// Written by Julian Maiorino, Michelle Clark
+// CSCI 230 Spring 2025
+// College of Charleston 
+
+// holds data for tower stack
+public class StackArray <T>
+{
+    private Object[] stackArr;
+    private int maxSize;
+    private int head;
+
+    public StackArray(int size)
+    {
+        maxSize = size;
+        stackArr = new Object[maxSize];
+        head = -1;
+    }
+
+    public boolean isFull()
+    {
+        return head == maxSize - 1;
+    }
+
+    public boolean isEmpty()
+    {
+        return head == -1;
+    }
+
+    public void push(T item)
+    {
+        if(isFull())
+            return;
+        stackArr[++head] = item;
+    }
+
+    public T pop()
+    {
+        if(isEmpty())
+        {
+            System.out.println("Stack is empty!"); // no exception throw as of now to avoid crashing program, 
+                                                     //but could add one
+            return null;
+        }
+        return (T) stackArr[head--];
+        
+    }
+
+    public T peek()
+    {
+        if(isEmpty())
+        {
+            System.out.println("Stack is empty!");
+            return null;
+        }
+        return (T) stackArr[head];
+    }
+}
