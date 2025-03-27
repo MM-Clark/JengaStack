61497a25245e26b1b180bcf82811d7af050ca2fa
MM-Clark <142937692+MM-Clark@users.noreply.github.com>
2025-03-27 07:50:05 -0400

    Adding end screen things
    New potential end image. Changed background sound for now because my lisp was annoying me.

--- STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/src/WinLoseScreen.java
+++ STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/src/WinLoseScreen.java
@@ -21,16 +21,20 @@ public class WinLoseScreen {
     //************* must add images to outside src folder + use absolute path b/c github ****************/
     private String[] loseImagePaths = {
             "STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/images/RedEyedFace.jpg",
-            "STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/images/StretchFace.jpg"
+            "STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/images/StretchFace.jpg",
+            "STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/images/ThreeRedFigures.jpg",
+            "STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/images/testerImg.jpg" // MUST BE LAST PATH FOR DEMO PURPOSES
     }; //STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025\RedEyedFace.jpg
     private String winImagePath = "STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/images/testerImg.jpg"; 
 
     // tried to do audio built in -- needs wav files
     private String[] audioPaths = {
+        "STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/sounds/StaticAudio.wav", // royalty free sound on YouTube from Dar Golan
         "STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/sounds/JengaStackRec1_Reversed.wav",
         "STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/sounds/QuickFoxLazyDog.wav"
     };
 
+    Clip clip;
     // may add audio if have time here as path --> currently have two mp3 files 
     // QuickFoxLazyDog is "The quick fox jumped over the lazy dog" distorted, reversed, equalized, slowed
     // SallySeashells is "Sammy collects seashells by the seashores on Sundays" distorted, reversed, inverted, equalized, slowed
@@ -40,6 +44,10 @@ public class WinLoseScreen {
     
     public void showWinScreen(JFrame frame) throws IOException
     {
+        //--------------------------------------------------------
+        // show winning screen -- Do people win Jenga? If so, how?
+        //--------------------------------------------------------
+
         // creating as BufferedImage to resize
         File imageFile = new File(winImagePath);
         BufferedImage originalImage = ImageIO.read(imageFile);
@@ -58,10 +66,14 @@ public class WinLoseScreen {
     
     public void showLoseScreen(JFrame frame) throws IOException
     {
+        //--------------------------------------------------------
+        // Show random jumpscare screen
+        //--------------------------------------------------------
+
         // randomization, works w/ two images as tested
         Random random = new Random();
-        int randomIndex = random.nextInt(loseImagePaths.length);
-        String randomImagePath = loseImagePaths[randomIndex];
+        int randomIndex = random.nextInt(loseImagePaths.length-1);
+        String randomImagePath = loseImagePaths[randomIndex]; // change randomIndex to loseImagePaths.length-1 for demo if need to avoid disturbing imagery
 
         // creating as BufferedImage to resize
         File imageFile = new File(randomImagePath);
@@ -84,31 +96,39 @@ public class WinLoseScreen {
 
     public void playRandomGameSound()
     {
-        Timer audioTimer = new Timer(1000, new ActionListener()
+        //-------------------------------------------------------------
+        //   Get audio path for background gameplay sound
+        //-------------------------------------------------------------
+
+        // play random game sound
+        // Random random = new Random();
+        // int randomIndex = random.nextInt(audioPaths.length);
+        String audioPath = audioPaths[0];
+        try {
+            File soundFile = new File(audioPath);
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
+            clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+            // Keep the program running until the sound finishes playing
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+            ex.printStackTrace();
+        }
+
+        //------------------------------------------------------------------------------------
+        // ------------------   AUDIO TIMER    -----------------------------------------------
+        //------------------------------------------------------------------------------------
+        // timer on millisecond delay
+        Timer audioTimer = new Timer(45000, new ActionListener() // clip is 45 sec. long; 45 seconds = 45,000 milliseconds
         {
             @Override
             public void actionPerformed(ActionEvent e)
             {
-                // play random game sound
-                Random random = new Random();
-                int randomIndex = random.nextInt(audioPaths.length);
-                String randomAudioPath = audioPaths[randomIndex];
-
-                try {
-                    File soundFile = new File(randomAudioPath);
-                    AudioInputStream audioIn = AudioSystem.getAudioInputStream(soundFile);
-                    Clip clip = AudioSystem.getClip();
-                    clip.open(audioIn);
-                    clip.start();
-                    // Keep the program running until the sound finishes playing
-                } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
-                    ex.printStackTrace();
-                }
+                clip.start();
             }
         });
 
         //start the timer after it's been created
         audioTimer.start();
-        
     }
 }
