73e6b543be510e5eeed6b793e46b35333a67a163
MM-Clark <142937692+MM-Clark@users.noreply.github.com>
2025-04-09 17:12:38 -0400

    get

--- STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/src/Randomizer.java
+++ STACK_GAME_FINAL_PROJECT_CSCI_230_CofC_Spring2025/src/Randomizer.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class Randomizer
 {
-    public int randomNumber(int min, int max)
+    public int getRandomNumber(int min, int max)
     {
         Random random = new Random(); 
         int number = random.nextInt((max-min) + 1) + min;
